baa6aca6524a00e7e11c7cb75fe29c9fbb2a1453
PROONGIT <136971797+PROONGIT@users.noreply.github.com>
2026-09-09 23:20:24 +0000

    Modify column distribution and comment markdown
    Adjusted column layout and commented out a markdown line.

--- app.py
+++ app.py
@@ -42,8 +42,8 @@ with st.sidebar:
     theme = st.session_state.theme
     T = THEMES[theme]
     
-    st.markdown("---")
-    col1, col2, col3 = st.columns([2, 3, 1])
+    # st.markdown("---")
+    col1, col2, col3 = st.columns([1, 1, 1])
     with col2:
         st.image("assets/sceau_togo.png", width=90)
     st.markdown(
